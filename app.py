8a18ccdafc078285b5e16c216933048176409177
hsnylmz1837 <hsnylmz1837@gmail.com>
2025-09-02 15:21:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -451,7 +451,7 @@ else:
 
                 # Adjacent support (only within a section)
                 adj = str(fld.get("Adjacent")).strip().lower() in ("true", "1", "yes")###
-                print adj
+                print(adj)
                 chain += piece
                 
                   
