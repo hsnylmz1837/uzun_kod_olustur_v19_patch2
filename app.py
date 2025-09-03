c160b4675fc73282e57fa11ba485d8f5996ad0d2
hsnylmz1837 <hsnylmz1837@gmail.com>
2025-09-03 16:03:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -243,10 +243,11 @@ elif st.session_state["step"]==2:
 else:
     s1, s2 = st.session_state["s1"], st.session_state["s2"]
     st.markdown('<div class="panel">', unsafe_allow_html=True)
-    st.header("Aşama 3 — Ürün ve Detay 🔗")
+    #st.header("Makina Tipini seçiniz:")
     #st.write(f"Seçimler: **{s1} → {s2}**")
     st.write("**Seçimler:**")
     st.write(f"**>> {s1}  >>  {s2} >>**")
+    st.write("Makina Tipini seçiniz:")
     prods=schema["products"].query("Kategori1==@s1 and Kategori2==@s2")
     if prods.empty:
         st.warning("Bu seçim için 'products' sayfasında satır yok.")
